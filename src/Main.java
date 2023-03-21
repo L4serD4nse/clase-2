f17ef501de7799e0d38ad48d0af1561262dca25b
santiago <santiagobocajrs452@gmail.com>
2023-03-20 21:55:26 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroinicio= 2;
+        int numerofin= 12;
+        boolean pares=true;
+
+        while(numeroinicio<= numerofin){
+            if(numeroinicio % 2 ==0 && pares) {
+                System.out.println(numeroinicio);
+            }
+            if(numeroinicio % 2== 1 && !pares){
+                System.out.println(numeroinicio);
+            }
+            numeroinicio++;
+            }
+        }
+    }
